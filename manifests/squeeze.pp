ec24d5610dc81e86f1746b2554b1acbb7b3b45f3
Billiam <billiamthesecond@gmail.com>
2013-07-16 14:03:26 -0500

    Updating directory permissions

--- manifests/squeeze.pp
+++ manifests/squeeze.pp
@@ -18,6 +18,20 @@ class squeeze {
   class { 'apache':
     default_vhost => false,
   }
+
+  exec { "/bin/chown root:www-data /var/www":
+    unless => "/bin/sh -c '[ $(/usr/bin/stat -c %G /var/www) == www-data ]'",
+  }
+  exec { "/bin/chmod 755 /var/www":
+    unless => "/bin/sh -c '[ $(/usr/bin/stat -c %a /var/www) == 755 ]'",
+  }
+
+  exec {"vagrant www-data membership":
+    unless => 'groups vagrant | grep -q "\bwww-data\b"',
+    command => "usermod -aG www-data vagrant",
+    require => [Class['apache']]
+  }
+
   apache::listen { '192.168.56.21:80': }
 
 
